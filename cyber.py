b59e3ceb394d152be14b4d7dac405bdac32c593f
gangstabatyr <114847268+gangstabatyr@users.noreply.github.com>
2024-05-17 03:17:42 +0600

    Update cyber.py

--- cyber.py
+++ cyber.py
@@ -13,10 +13,12 @@ classifier=pickle.load(pickle_in)
 
 
 
-def predict_note_authentication(packet_length, num_packets, icmp,
-                                tcp, udp):
-    prediction = classifier.predict([[packet_length, num_packets, icmp,
-                                      tcp, udp]])
+def predict_note_authentication(packet_length, num_packets, protocol):
+    prediction = classifier.predict([[packet_length, num_packets, protocol]])
+    icmp = 1 if protocol == 'ICMP' else 0
+    tcp = 1 if protocol == 'TCP' else 0
+    udp = 1 if protocol == 'UDP' else 0
+    prediction = classifier.predict([[packet_length, num_packets, icmp, tcp, udp]])
     print(prediction)
     return prediction
 
@@ -24,14 +26,11 @@ def main():
     st.title("Analysis of network threat")
     packet_length = st.number_input('how long is length packet', step=0.1, value=0.0)
     num_packets = st.number_input('how many packets you have?', step=0.1, value=0.0)
-    icmp = st.radio('does your packet has icmp (0 - no, 1 - yes)', (0, 1))
-    tcp = st.radio('does your packet has tcp (0 - no, 1 - yes)', (0, 1))
-    udp = st.radio('does your packet has udp (0 - no, 1 - yes)', (0, 1))
+    protocol = st.radio('Select your packet protocol', ('ICMP', 'TCP', 'UDP'))
 
     result = ""
     if st.button("Predict"):
-        result = int(predict_note_authentication(packet_length, num_packets, icmp,
-                                                 tcp, udp))
+        result = int(predict_note_authentication(packet_length, num_packets, protocol))
         if result == 0:
             with st.spinner('Wait for it...'):
                 time.sleep(2)
