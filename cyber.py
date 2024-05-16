ef22a725f02cd8f54d9f924583060a2979930aff
gangstabatyr <114847268+gangstabatyr@users.noreply.github.com>
2024-05-17 02:55:52 +0600

    Update cyber.py

--- cyber.py
+++ cyber.py
@@ -23,18 +23,18 @@ def main():
     st.title("Analysis of network threat")
     packet_length = st.number_input('how long is length packet', step=0.1, value=0.0)
     num_packets = st.number_input('how many packets you have?', step=0.1, value=0.0)
-    icmp = st.radio('does your packet has icmp (0 - no, 1 - yes ', (0, 1))
-    tcp = st.radio('does your packet has tcp (0 - no, 1 - yes ', (0, 1))
-    udp = st.radio('does your packet has udp (0 - no, 1 - yes ', (0, 1))
+    icmp = st.radio('does your packet has icmp (0 - no, 1 - yes)', (0, 1))
+    tcp = st.radio('does your packet has tcp (0 - no, 1 - yes)', (0, 1))
+    udp = st.radio('does your packet has udp (0 - no, 1 - yes)', (0, 1))
 
     result = ""
     if st.button("Predict"):
         result = int(predict_note_authentication(packet_length, num_packets, icmp,
                                                  tcp, udp))
         if result == 0:
-            st.success('You have risk of network threat')
-        else:
-            st.success('You dont have risk of network threat')
+            st.success('You have risk of network threat').markdown(f'<p style="background-color:#FF0000)
+        else if result == 1:
+            st.success('You don't have risk of network threat')
 
 if __name__ == '__main__':
     main()
